a91606f9ea75157b2da3e1fadb462fe7b267e743
Vibhavasu Bhadauria <vibhavasudsvv2003@gmail.com>
2024-09-19 16:21:57 +0530

    version 1

---
+++ Functions.py
@@ -0,0 +1,7 @@
+l1=[4,6,2,8,1,9]
+#printing length of list
+print(f'Length of list : {len(l1)}')
+
+#printing maximum and minimum item of list
+
+print(f'Maximum item : {max(l1)}\nMinimum item : {min(l1)}')
