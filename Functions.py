730e74e043ec4694c04a77c1a2a350b4087b14cc
Vibhavasu Bhadauria <138148506+vibhavasubhadauria2003@users.noreply.github.com>
2024-09-23 15:33:55 +0530

    version 2

--- Functions.py
+++ Functions.py
@@ -5,3 +5,7 @@ print(f'Length of list : {len(l1)}')
 #printing maximum and minimum item of list
 
 print(f'Maximum item : {max(l1)}\nMinimum item : {min(l1)}')
+
+print(f'Sum of list is : {sum(l1)}')
+
+print(f'Sorted list is : {sorted(l1)}')
